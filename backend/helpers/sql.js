df743b473cd11f1cbe7828893cbbd0957aad6b6e
eduardopedroza <ea.pedroza23@gmail.com>
2024-03-20 09:12:33 -0600

    added helper function for updating database

---
+++ backend/helpers/sql.js
@@ -0,0 +1,33 @@
+const { BadRequestError } = require("../expressError");
+
+/**
+ * Creates SQL SET clause and values for a partial update query.
+ *
+ * This function takes an object containing data to update and a mapping object
+ * for converting JavaScript-style keys to SQL column names. It returns an object
+ * with `setCols` and `values` properties to be used in an SQL query.
+ *
+ * @param {Object} dataToUpdate - The data to update in the form of {key: value}.
+ * @param {Object} columnMapping - An object mapping JavaScript-style keys to SQL column names.
+ * @returns {Object} An object with `setCols` and `values` properties.
+ * @throws {Error} Throws an error if no data is provided.
+ */
+
+function createSqlSetClause(dataToUpdate, columnMapping) {
+  // Check if dataToUpdate is empty
+  if (Object.keys(dataToUpdate).length === 0) {
+    throw new Error("No data provided");
+  }
+
+  // Convert dataToUpdate to SQL SET clause format
+  const setCols = Object.entries(dataToUpdate).map(([key, value], index) => {
+    const columnName = columnMapping[key] || key;
+    return `"${columnName}"=$${index + 1}`;
+  });
+
+  const values = Object.values(dataToUpdate);
+
+  return { setCols: setCols.join(", "), values };
+}
+
+module.exports = { createSqlSetClause };
