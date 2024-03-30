10bd62ffadb9a1980cbb486ca62cbef74443ed7f
eduardopedroza <ea.pedroza23@gmail.com>
2024-03-30 16:15:19 -0600

    created sellers routes and tests

---
+++ backend/routes/sellers.js
@@ -0,0 +1,101 @@
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const router = new express.Router();
+const Seller = require("../models/seller");
+const { createToken } = require("../helpers/tokens");
+const { BadRequestError } = require("../expressError");
+
+const sellerNewSchema = require("../schemas/sellersNewSchema.json");
+const sellerUpdateSchema = require("../schemas/sellersUpdateSchema.json");
+
+/** POST /register { seller } => { seller }
+ *
+ * seller should be { firstName, lastName, username, email, password
+ *                  shippingAddress, companyName, contactInfo }
+ *
+ * Returns { sellerId, companyName, contactInfo }
+ *
+ * Authorization required: none
+ */
+
+router.post("/register", async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, sellerNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const seller = await Seller.create(req.body);
+    const token = createToken(seller.sellerId);
+    return res.status(201).json({ token });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+/** GET /[sellerId]  =>  { seller }
+ *
+ *  Seller is { companyName, contactInfo, rating, salesCount }
+ *
+ *
+ * Authorization required: none
+ */
+router.get("/:sellerId", async (req, res, next) => {
+  try {
+    const seller = await Seller.getSellerWithId(req.params.sellerId);
+    return res.json({ seller });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+/** PATCH /[sellerId] { password, firstName, lastName, email, shippingAddress,
+ *                    companyName, contactInfo } => { seller }
+ *
+ * Patches seller data.
+ *
+ * Returns {  username, firstName, lastName, email, shippingAddress
+ *           companyName, contactInfo, rating, salesCount }
+ *
+ * Authorization required: none
+ */
+
+router.patch("/:sellerId", async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, sellerUpdateSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const seller = await Seller.update(req.params.sellerId, req.body);
+    return res.json({ seller });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** PATCH /[sellerId]
+ *
+ * Hides seller from application
+ *
+ * Returns undefined
+ *
+ * Authorization required: none
+ */
+
+router.patch("/:sellerId/remove", async (req, res, next) => {
+  try {
+    await Seller.remove(req.params.sellerId);
+    return res.json({
+      user: req.params.sellerId,
+      deleted: true,
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
---
+++ backend/routes/sellers.test.js
@@ -0,0 +1,152 @@
+const request = require("supertest");
+
+const db = require("../db.js");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+  u2Token,
+  getSellerOneId,
+} = require("./_testCommon");
+const { NotFoundError } = require("../expressError.js");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /sellers/register */
+
+describe("POST /sellers/register", () => {
+  const newSeller = {
+    username: "test s1",
+    firstName: "test S1F",
+    lastName: "test S1L",
+    email: "testseller1@seller.com",
+    password: "testpassword1",
+    shippingAddress: "test address 4",
+    companyName: "Test Seller 1",
+    contactInfo: "858-222-2232",
+  };
+
+  test("works", async () => {
+    const response = await request(app)
+      .post(`/sellers/register`)
+      .send(newSeller);
+    expect(response.body).toHaveProperty("token");
+  });
+
+  test("bad request with missing fields", async () => {
+    const response = await request(app).post("/sellers/register").send({
+      username: "new",
+    });
+    expect(response.statusCode).toEqual(400);
+  });
+
+  test("bad request with invalid data", async () => {
+    const response = await request(app).post("/sellers/register").send({
+      username: "new",
+      firstName: "first",
+      lastName: "last",
+      password: "password",
+      email: "fakeemail",
+      shippingAddress: "test address",
+    });
+    expect(response.statusCode).toEqual(400);
+  });
+});
+
+/************************************** GET /users/:sellerId */
+
+describe("GET /sellers/:sellerId", () => {
+  test("works", async () => {
+    const response = await request(app).get(`/sellers/${getSellerOneId()}`);
+    expect(response.body).toEqual({
+      seller: {
+        companyName: "Seller 1",
+        contactInfo: "858-222-2222",
+        rating: null,
+        salesCount: 0,
+      },
+    });
+  });
+
+  test("not found if no such user", async () => {
+    const response = await request(app).get(`/sellers/${92948}`);
+    expect(response.statusCode).toEqual(404);
+  });
+});
+
+/************************************** PATCH /sellers/:sellerId */
+
+describe("PATCH /sellers/:sellerId", () => {
+  test("works", async () => {
+    const response = await request(app)
+      .patch(`/sellers/${getSellerOneId()}`)
+      .send({
+        firstName: "test update",
+        lastName: "test update",
+        email: "testupdate@email.com",
+        shippingAddress: "test update",
+        companyName: "Updated Company Name",
+        contactInfo: "858-555-5555",
+      });
+    expect(response.body.seller).toEqual({
+      username: "s1",
+      firstName: "test update",
+      lastName: "test update",
+      email: "testupdate@email.com",
+      shippingAddress: "test update",
+      companyName: "Updated Company Name",
+      contactInfo: "858-555-5555",
+      rating: null,
+      salesCount: 0,
+    });
+  });
+
+  test("not found with no such user", async () => {
+    const response = await request(app).patch(`/sellers/${324212}`).send({
+      firstName: "test update",
+      lastName: "test update",
+      email: "testupdate@email.com",
+      shippingAddress: "test update",
+      companyName: "Updated Company Name",
+      contactInfo: "858-555-5555",
+    });
+    expect(response.statusCode).toEqual(404);
+  });
+
+  test("bad request with missing data", async () => {
+    const response = await request(app)
+      .patch(`/sellers/${getSellerOneId()}`)
+      .send({});
+    expect(response.statusCode).toEqual(400);
+  });
+});
+
+describe("PATCH /sellers/:sellerId/remove", () => {
+  test("works", async () => {
+    const response = await request(app).patch(
+      `/sellers/${getSellerOneId()}/remove`
+    );
+    const sellerRes = await db.query(
+      `SELECT * FROM sellers WHERE seller_id = $1 AND deleted = FALSE`,
+      [getSellerOneId()]
+    );
+    const userRes = await db.query(
+      `SELECT * FROM users WHERE user_id = $1 AND deleted = FALSE`,
+      [getSellerOneId()]
+    );
+    expect(sellerRes.rows.length).toEqual(0);
+    expect(userRes.rows.length).toEqual(0);
+  });
+
+  test("not found if no such seller", async () => {
+    const response = await request(app).patch(`/sellers/${1241212}/remove`);
+    expect(response.statusCode).toEqual(404);
+  });
+});
