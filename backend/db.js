14c53210401196d64f5e4c10ec1ddebf1a19af68
eduardopedroza <ea.pedroza23@gmail.com>
2024-05-16 06:33:23 -0600

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,21 @@
-const pg = require("pg");
+const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-const conString = getDatabaseUri();
-const db = new pg.Client({
-  connectionString: conString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+let db;
 
-db.connect(function (err) {
-  if (err) {
-    return console.error("could not connect to postgres", err);
-  }
-  db.query('SELECT NOW() AS "theTime"', function (err, result) {
-    if (err) {
-      return console.error("error running query", err);
-    }
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
-});
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+  });
+}
+
+db.connect();
 
 module.exports = db;
